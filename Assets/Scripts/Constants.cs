c209eaf258672017fde1e1faaa80e991b3f9edcc
Vladyslav Dmytrenko <94719332+dmytrenkoVlad@users.noreply.github.com>
2021-11-21 01:45:00 +0200

    Update Constants.cs

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -5,7 +5,7 @@ namespace Assets.Scripts
     public static class Constants
     {
         public const int DefaultDamage = 1;
-        public static Vector3 DefaultBallSpeed = new Vector3(10, 10);
+        public static Vector3 DefaultBallSpeed = new Vector3(8, 8);
         public static Vector2Int BricksFieldSize = new Vector2Int(14, 6);
         public const int PlatfromLayer = 8;
         public const int Attempts = 3;
