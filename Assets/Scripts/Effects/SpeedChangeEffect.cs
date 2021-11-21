983e49164f3de5708e1f5a4adefb0ba59458adbc
Vladyslav Dmytrenko <94719332+dmytrenkoVlad@users.noreply.github.com>
2021-11-21 02:08:35 +0200

    Updated speed changing

--- Assets/Scripts/Effects/SpeedChangeEffect.cs
+++ Assets/Scripts/Effects/SpeedChangeEffect.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 
 namespace Assets.Scripts.Effects
 {
-    public class SpeedIncreaseEffect : OnDestroyBaseEffectComponent
+    public class SpeedChangeEffect : OnDestroyBaseEffectComponent
     {
-        private static Vector2 IncreasedBallSpeed = Constants.DefaultBallSpeed * 1.5f;
+        private static Vector2 ChangeBallSpeed = Constants.DefaultBallSpeed * 0.7f;
 
         protected override float EffectDuration => 10f;
 
@@ -14,7 +14,7 @@ namespace Assets.Scripts.Effects
             if (!ballController.TryGetComponent(out BallMovementComponent ballMovement))
                 return;
 
-            ballMovement.SetNewMovementSpeed(IncreasedBallSpeed, EffectDuration);
+            ballMovement.SetNewMovementSpeed(ChangeBallSpeed, EffectDuration);
         }
     }
 }
